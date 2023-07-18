139b0da2f4b58b297749d574f301aa443292940d
husnuyasar <husnuyasar58@gmail.com>
2023-07-18 23:27:57 +0300

    🍺 Refactored beer client URL

--- order_service/clients/beer.client.js
+++ order_service/clients/beer.client.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
+require('dotenv').config()
 
 const PROTO_PATH = path.join(__dirname + "/../protos/beer.proto");
 
@@ -15,8 +16,8 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   const beerProto = grpc.loadPackageDefinition(packageDefinition).beer;
   
   const beerClient = new beerProto.Beer(
-    "0.0.0.0:3002",
+    `${process.env.BEER_SERVICE}:3002`,
     grpc.credentials.createInsecure()
   );
   
-  module.exports = beerClient;
+  module.exports = beerClient; 
