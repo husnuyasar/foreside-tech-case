bf57923b4e00ca8c2443691396f87aba31c0b95a
Hüsnü Yaşar <h.yasar@azerion.com>
2023-07-19 18:58:51 +0300

    🔥 Removed unnecessary comments

--- beer_service/start.sh
+++ beer_service/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /app/beer_service/
-# Run npm run db
+
 echo "Running npm run db..."
 npm run db
 
-# Run npm run migrate
+
 echo "Running npm run migrate..."
 npm run migrate
 
-# Run npm run seed
+
 echo "Running npm run seed..."
 npm run seed
 
